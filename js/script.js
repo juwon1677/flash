d86f4685d746455251f59474e2ba06735734bfe7
adeniran adejuwon <adeniran adejuwon>
2023-11-30 11:22:28 -0800

    add files and folders

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+$(".click").click(function() {
+$("#js-showing").toggle();
+$("js-hidden").toggle();
+})
+
+$(".click").click(function() {
+  $("#ks-showing").toggle();
+  $("ks-hidden").toggle();
+  })
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
