6fd05d1f32661d0ce2bbd1da06c1dd0217f1d00a
adeniran adejuwon <adeniran adejuwon>
2023-12-01 01:50:27 -0800

    add changes 3 4 5

--- js/script.js
+++ js/script.js
@@ -3,17 +3,33 @@ $(".click").click(function() {
 $("#js-showing").toggle();
 $("js-hidden").toggle();
 })
-
-$(".click").click(function() {
-  $("#js-showing").toggle();
-  $("js-hidden").toggle();
+$(".click-2").click(function() {
+  $("#operator-showing").toggle();
+  $("operator-hidden").toggle();
   })
 
-
-  $(".click").click(function() {
-    $("#js-showing").toggle();
-    $("js-hidden").toggle();
+  $(".click-3").click(function() {
+    $("#variable-showing").toggle();
+    $("variable-hidden").toggle();
     })
+
+    
+    $(".click-4").click(function() {
+      $("#convention-showing").toggle();
+      $("convention-hidden").toggle();
+      })
+      
+    $(".click-5").click(function() {
+      $("#function-showing").toggle();
+      $("function-hidden").toggle();
+      })
+
+      $(".click-6").click(function() {
+        $("#method-showing").toggle();
+        $("method-hidden").toggle();
+        })
+      
+    
 
 
 
